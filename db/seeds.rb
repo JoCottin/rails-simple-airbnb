5fb75a8c5d4ec978c922da83900680f8b3be839d
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-05-27 18:17:59 +0200

    rails airbnb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Jambonneau',
+  address: 'In the pig',
+  description: 'Tema le cochon',
+  price_per_night: 45,
+  number_of_guests: 23
+)
+
+Flat.create!(
+  name: 'Jackie Chan 2',
+  address: 'Meme adresse quavant',
+  description: 'Il est de retour mon pote pour mettre des tatanes a tatons',
+  price_per_night: 59,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Michel Leeb Return',
+  address: 'Il a migré au RN',
+  description: 'Elu endroit le plus beauf de france',
+  price_per_night: 1,
+  number_of_guests: 90
+)
